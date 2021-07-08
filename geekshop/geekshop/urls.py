0bbab97a71c2f7497140a5e5aebcf0439cc3baea
EsterMiracle <greedolife@yandex.ru>
2021-07-08 20:27:06 +0300

    add lesson 5

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -9,7 +9,7 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including another URL conf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
@@ -26,6 +26,7 @@ urlpatterns = [
     path('contacts/', contacts, name="contacts"),
     path('products/', include('mainapp.urls', namespace="products")),
     path('auth/', include('authapp.urls', namespace="auth")),
+    path('basket/', include('basketapp.urls', namespace="basket")),
 ]
 
 if settings.DEBUG:
