f15826d883ed4cc8341c774d4f206de60e5a8a8b
EsterMiracle <greedolife@yandex.ru>
2021-07-19 23:51:27 +0300

    add lesson 8

---
+++ geekshop/adminapp/templatetags/my_tags.py
@@ -0,0 +1,14 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.filter(name='media_folder_products')
+def media_folder_products(string):
+    if not string:
+        string = 'product_images/default.jpg'
+    return f'{settings.MEDIA_URL}{string}'
+
+
+register.filter('media_folder_products', media_folder_products)
